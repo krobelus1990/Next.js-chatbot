7b728c3dcbf83bd9ad468e834a0e0104789dbd3c
Ninja0930 <resolution0930@gmail.com>
2024-11-11 16:39:59 +0900

    fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,21 +1,18 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Background from "./components/atoms/Background";
-import favicon from "../assets/UNYJK-180x180.png";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Resolution Chatbot",
-  description: "Resolution Chatbot",
+  title: "Alexandru Chatbot",
+  description: "Alexandru Chatbot",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href={favicon.src} type="image/png" sizes="any" />
-      </head>
+      <head></head>
       <body className={inter.className}>
         <div className=" fixed z-[-1]">
           <Background />
